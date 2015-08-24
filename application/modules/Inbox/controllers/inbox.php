e2998e7d0d745d57ce31c05e82811377ee8c8956
nuris akbar <nuris.akbar@gmail.com>
2015-08-24 14:24:35 +0700

    first commit

---
+++ application/modules/Inbox/controllers/inbox.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+ 
+class Inbox extends MX_Controller {
+	/**
+		* @Author				: Localhost {Ferdhika Yudira}
+		* @Email				: fer@dika.web.id
+		* @Web					: http://dika.web.id
+		* @Date					: 2015-02-21 08:31:25
+	**/
+	public function index(){
+		echo "ini beranda";
+	}
+}
